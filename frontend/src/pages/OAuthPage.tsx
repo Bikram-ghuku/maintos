1c70c15827b5f61bf002ac2ac87189d5cbddf393
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2026-02-13 14:01:55 +0530

    fix: prevent double requests

--- frontend/src/pages/OAuthPage.tsx
+++ frontend/src/pages/OAuthPage.tsx
@@ -7,40 +7,47 @@ import { Header } from "../components/Common/Common";
 export default function OAuthPage() {
 	const auth = useAuthContext();
 	const navigate = useNavigate();
-	const [message, setMessage] = useState<string>("Authenticating, please wait...");
+	const [message, setMessage] = useState<string>(
+		"Authenticating, please wait...",
+	);
 	const [awaitingResponse, setAwaitingRepsonse] = useState<boolean>(false);
 
 	const loginHandler = async (code: string) => {
-		const response = await makeRequest('oauth', 'post', { code });
+		if (!awaitingResponse) {
+			setAwaitingRepsonse(true);
+			const response = await makeRequest("oauth", "post", { code });
 
-		if (response.status === 'success') {
-			if ("token" in response.data) {
-				auth.login(response.data["token"]);
-				setMessage("Successfully authenticated.");
-				navigate('/');
+			if (response.status === "success") {
+				if ("token" in response.data) {
+					auth.login(response.data["token"]);
+					setMessage("Successfully authenticated.");
+					navigate('/');
+				} else {
+					setMessage("Authentication failed.");
+				}
+			} else {
+				setMessage(
+					"Authentication failed due to a server error. Please try again later.",
+				);
 			}
-			else {
-				setMessage("Authentication failed.");
-			}
-		} else {
-			setMessage("Authentication failed due to a server error. Please try again later.");
+
+			setAwaitingRepsonse(false);
 		}
-
-		setAwaitingRepsonse(false);
-	}
+	};
 
 	useEffect(() => {
-		if (auth.isAuthenticated) {
-			setMessage("User already authenticated.");
-			navigate('/');
-		} else {
-			const urlParams = new URLSearchParams(location.search);
+		if (!awaitingResponse) {
+			if (auth.isAuthenticated) {
+				setMessage("User already authenticated.");
+				navigate('/');
+			} else {
+				const urlParams = new URLSearchParams(location.search);
 
-			if (urlParams.get("code") === null) {
-				setMessage("No OAuth code found.");
-			} else if (!awaitingResponse) {
-				setAwaitingRepsonse(true);
-				loginHandler(urlParams.get("code") as string);
+				if (urlParams.get("code") === null) {
+					setMessage("No OAuth code found.");
+				} else if (!awaitingResponse) {
+					loginHandler(urlParams.get("code") as string);
+				}
 			}
 		}
 	}, []);
@@ -48,7 +55,11 @@ export default function OAuthPage() {
 	return (
 		<Header
 			title="Admin OAuth"
-			subtitle={awaitingResponse ? "Authenticating with the server, please wait." : message}
+			subtitle={
+				awaitingResponse
+					? "Authenticating with the server, please wait."
+					: message
+			}
 		/>
-	)
+	);
 }
