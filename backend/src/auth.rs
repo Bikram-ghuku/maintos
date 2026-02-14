06ba8d26b3dcd7b55d1c7cb3dce7430b707d45f1
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2026-02-14 14:34:55 +0530

    feat: use `jsonwebtoken` crate and ensure token expiry is checked

--- backend/src/auth.rs
+++ backend/src/auth.rs
@@ -2,9 +2,8 @@
 //!
 //! Currently this is only used in the admin dashboard and uses Github OAuth for authentication
 
-use anyhow::anyhow;
-use jwt::{Claims, RegisteredClaims, SignWithKey, VerifyWithKey};
-use std::collections::BTreeMap;
+use jsonwebtoken::{self, Header, Validation};
+use serde::{Deserialize, Serialize};
 
 use crate::{env::EnvVars, github, utils::Res};
 
@@ -16,76 +15,54 @@ pub struct Auth {
     pub gh_access_token: String,
 }
 
+/// Struct containing the JWT claims
+#[derive(Clone, Serialize, Deserialize)]
+struct JWTClaims {
+    /// The Github username of the user
+    username: String,
+    /// Github access token for the user
+    /// WARNING: This token is stored in the claims UNENCRYPTED. For this SPECIFIC USECASE, the token is only used to check org membership and repository access.
+    /// The tokens are FINE GRAINED and NO OTHER permissions are granted. DO NOT USE this in case the tokens are sensitive.
+    gh_access_token: String,
+    /// Issued at time
+    iat: usize,
+    /// Expiry
+    exp: usize,
+}
+
 /// Verifies whether a JWT is valid and signed with the secret key
 ///
 /// Returns the username and jwt in a struct
 pub async fn verify_token(token: &str, env_vars: &EnvVars) -> Res<Auth> {
-    let jwt_key = env_vars.get_jwt_key()?;
-    let claims: Result<Claims, _> = token.verify_with_key(&jwt_key);
+    let (_, jwt_key) = env_vars.get_jwt_key();
+    let validation = Validation::default();
 
-    let claims = claims.map_err(|_| anyhow!("Claims not found on the JWT."))?;
-    let username = claims
-        .private
-        .get("username")
-        .ok_or("Username not in the claims.")
-        .map_err(|_| anyhow!("Username not in the claims."))?;
-    let username = username
-        .as_str()
-        .ok_or("Username is not a string.")
-        .map_err(|_| anyhow!("Username is not a string."))?
-        .to_owned();
-
-    let gh_access_token = claims
-        .private
-        .get("gh_access_token")
-        .ok_or("Github access token not in the claims.")
-        .map_err(|_| anyhow!("Github access token not in the claims."))?;
-    let gh_access_token = gh_access_token
-        .as_str()
-        .ok_or("GH access token is not a string.")
-        .map_err(|_| anyhow!("GH access token is not a string."))?
-        .to_owned();
+    let claims = jsonwebtoken::decode::<JWTClaims>(token, &jwt_key, &validation)?.claims;
 
     Ok(Auth {
         jwt: token.to_owned(),
-        username,
-        gh_access_token,
+        username: claims.username,
+        gh_access_token: claims.gh_access_token,
     })
 }
 
 /// Generates a JWT with the username (for claims) and secret key
 async fn generate_token(username: &str, gh_access_token: &str, env_vars: &EnvVars) -> Res<String> {
-    let jwt_key = env_vars.get_jwt_key()?;
+    let (jwt_key, _) = env_vars.get_jwt_key();
 
     let now = chrono::Utc::now();
     let expiry = (now + chrono::Duration::hours(4)).timestamp(); // Github access tokens expire in 8 hours
     let issued_at = now.timestamp();
 
-    let mut private_claims = BTreeMap::new();
-    private_claims.insert(
-        "username".into(),
-        serde_json::Value::String(username.into()),
-    );
-    private_claims.insert(
-        "gh_access_token".into(),
-        serde_json::Value::String(gh_access_token.into()),
-    );
-
-    let claims = Claims {
-        registered: RegisteredClaims {
-            audience: None,
-            issuer: None,
-            subject: None,
-            not_before: None,
-            json_web_token_id: None,
-
-            issued_at: Some(issued_at as u64),
-            expiration: Some(expiry as u64),
-        },
-        private: private_claims,
+    let claims = JWTClaims {
+        iat: issued_at as usize,
+        exp: expiry as usize,
+        username: username.to_owned(),
+        gh_access_token: gh_access_token.to_owned(),
     };
 
-    Ok(claims.sign_with_key(&jwt_key)?)
+    let header = Header::default();
+    Ok(jsonwebtoken::encode(&header, &claims, &jwt_key)?)
 }
 
 /// Takes a Github OAuth code and creates a JWT authentication token for the user
